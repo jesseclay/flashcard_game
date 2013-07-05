a055e80c740c2079d97e678ce1897e2ce04248c9
Apprentice <apprentice@dbc07.local>
2013-07-05 11:13:09 -0700

    added card class and view (not finished) to dir

---
+++ card_class.rb
@@ -0,0 +1,10 @@
+class Card 
+
+  attr_reader :definition, :term 
+
+  def initialize(definition, term)
+    @definition = definition
+    @term = term
+  end
+
+end
---
+++ card_view.rb
@@ -0,0 +1,12 @@
+new_flashcard_game = Flashcard.new
+
+
+puts "Welcome to #{new_flashcard_game.name}!  To play enter the right term for each definition."
+puts "Let's get ready to rumble!!!!"
+
+if answer.downcase == 'yes' || 'y'
+  while.(new_flashcard_game.(arrayname).any?)
+
+
+  end
+end
