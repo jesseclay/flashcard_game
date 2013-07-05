4302a53be30e6c005c6c8700dd38ca6a55424065
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 12:03:59 -0700

    made view and controller compatable

--- card_class.rb
+++ card_class.rb
@@ -2,9 +2,52 @@ class Card
 
   attr_reader :definition, :term 
 
-  def initialize(definition, term)
-    @definition = definition
-    @term = term
+  def initialize(args)
+    @definition = args.fetch(:definition)
+    @term = args.fetch(:term)
   end
 
+end
+
+class CardDeck
+  attr_accessor :name
+
+  def initialize(args)
+    @game_name = args.fetch(:deck_name)
+    @deck = populate_deck
+  end
+
+  def populate_deck
+    CSV.foreach(@game_name) do |term,definition|
+      Card.new({:term => term, :definition => definition})
+    end
+  end
+
+  def shuffle_deck
+  end
+
+  def all_terms_guessed
+    # all of the cards have been guessed correctly
+    deck.empty?
+  end
+
+  def compare_answer
+  end
+
+  def current_definition
+  end
+
+  def current_term
+  end
+
+  def card_guessed_incorrectly
+    # current card put to the back of the array, updates
+    # current_term and current_definition to the next card
+    # in the array
+  end
+
+  def card_guessed_correctly
+    # current card taken out of array, updates current_term and
+    # current_definition to next card in array.
+  end
 end
--- card_view.rb
+++ card_view.rb
@@ -7,15 +7,14 @@ puts "Let's get ready to rumble!!!!  READY?"
 answer = gets.chomp.downcase
 
 if answer == 'yes' || answer == 'y'
-  while.(new_flashcard_game.(arrayname).any?)
-    #GET definition and term from card
+  until new_flashcard_game.all_terms_guessed
     puts "Definition:"
-    puts #{}"#{(array.definition)}"  
-    guess = gets guess.chomp 
-    if guess == #term
-      #REMOVE card from deck and end loop
+    puts #{}"#{new_flashcard_game.current_definition}"  
+    guess = gets guess.chomp
+    if guess == new_flashcard_game.current_term
+      new_flashcard_game.card_guessed_correctly
     else
-      #REMOVE card from deck and end loop
+      new_flashcard_game.card_guessed_incorrectly
     end 
   end
 else
